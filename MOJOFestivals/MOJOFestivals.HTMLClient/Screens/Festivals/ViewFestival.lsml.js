1ab5ac4af507a3365ad213c42a9b8b17b67aee6d
Frosit <fabioros.it@gmail.com>
2016-03-26 14:36:27 +0100

    working html client

--- MOJOFestivals/MOJOFestivals.HTMLClient/Screens/Festivals/ViewFestival.lsml.js
+++ MOJOFestivals/MOJOFestivals.HTMLClient/Screens/Festivals/ViewFestival.lsml.js
@@ -9,7 +9,36 @@ myapp.ViewFestival.Details_postRender = function (element, contentItem) {
 }
 
 
-myapp.ViewFestival.TicketsResterend_postRender = function (element, contentItem) {
-    // Write code here.
+myapp.ViewFestival.jsTicketsResterend_postRender = function (element, contentItem) {
+    function calculateTickets(tickets) {
+        var total = 0;
+        for (i = 0; i < tickets.length; i++) {
+            total = total + tickets[i]["Aantal"];
+        }
+        return total;
+    }
+
+    function updateTicketsResterend() {
+        var ticketsVerkocht = contentItem.screen.getTickets().then(function (result) {
+            totaalVerkocht = calculateTickets(result.data);
+
+            var totaalMsg = "";
+            
+            var totaalTickets = contentItem.screen.Festival.TicketsTotaal;
+            if (totaalVerkocht < totaalTickets) {
+                
+                totaalMsg = "Er zijn nog " + String( totaalTickets - totaalVerkocht) + " ticket(s) over.";
+                console.log(totaalVerkocht);
+            } else {
+                totaalMsg = "Helaas, we zijn uitverkocht";
+            }
+            
+
+            contentItem.screen.jsTicketsResterend = totaalMsg;
+            //return totaal;
+        });
+    }
+
+    contentItem.dataBind("screen.jsTicketsResterend", updateTicketsResterend);
 
 };
